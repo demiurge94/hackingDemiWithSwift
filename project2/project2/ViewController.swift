73e2c9d87a19f5b47e8e83340207aa99e3b10da2
demiurge94 <erickvicencio@gmail.com>
2021-07-02 16:21:49 -0700

    finished project 2 and challenge exercises from wrap-up

--- project2/project2/ViewController.swift
+++ project2/project2/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     var countries = [String]()
+    var correctAnswer = 0
+    var questionsAsked = 0
     var score = 0
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,16 +31,60 @@ class ViewController: UIViewController {
         countries.append("US")
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
-        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        /*CALayer (core animation layer) is lower than UIbutton so doesn't know
+          what UIcolor is, so we call a UIcolor light gray, then so it matches
+          the type, we converted it to CGColor. */
         askQuestion()
+        
+        
         // Do any additional setup after loading the view.
     }
-    func askQuestion(){
+    //UIAlertAction is required for the alert we do after tapping, in which one of the parameters needs a method that accepts this type as a parameter
+    func askQuestion(action: UIAlertAction! = nil){
+        countries.shuffle()
+        if questionsAsked == 10{
+            questionsAsked = 0
+            score = 0
+        }
+        correctAnswer = Int.random(in: 0...2)
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = Int.random(in: 0...2)
+        title = "\(countries[correctAnswer].uppercased())    score: \(score) "
+        questionsAsked += 1
     }
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer{
+            title = "Correct"
+            score += 1
+        }
+        else{
+            title = "Wrong, that's the flag of \(countries[sender.tag]) "
+            score += 0
+        }
+        if questionsAsked != 10 {
+        let ac = UIAlertController(title: title, message: "Your Score is \(score).", preferredStyle: .alert)
+        //can use .alert or .actionsheet. alert is for showing people changes, latter is for showing a list of options.
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion)) //if we use askQuestion() it is telling the compiler "run this now, and IT will tell you the name of the method to run.
+        present(ac, animated: true)
+        }
+        
+        if questionsAsked == 10{
+            let ac = UIAlertController(title: title, message: "Your Score is \(score) out of 10.", preferredStyle: .alert)
+            
+            //can use .alert or .actionsheet. alert is for showing people changes, latter is for showing a list of options.
+            
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion)) //if we use askQuestion() it is telling the compiler "run this now, and IT will tell you the name of the method to run.
+            present(ac, animated: true)
+        }
+    }
+    
 
 }
 
